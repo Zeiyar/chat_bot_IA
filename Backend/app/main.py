806811305d62f1de32a5d18a4d057da8a27142c2
Zeiyar <marieycorentin97@gmail.com>
2026-02-03 15:48:30 +0100

    [backend]feature: authorization header pour le cors

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -3,8 +3,17 @@ from app.auth.routes import router as auth_router
 from app.routes.user import router as user_router
 from app.routes.history import router as history_router
 from app.routes.ai import router as ai_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Vite
+    allow_credentials=True,                   # 🔑 cookies
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(ai_router)
 app.include_router(history_router)
