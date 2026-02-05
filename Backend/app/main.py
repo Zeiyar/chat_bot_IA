e7e3457ae1f2bc4acd9d0d72c32901cc0771ecb7
Zeiyar <marieycorentin97@gmail.com>
2026-02-05 12:37:58 +0100

    [backend]refacto:deleting history db

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from app.auth.routes import router as auth_router
 from app.routes.user import router as user_router
-from app.routes.history import router as history_router
 from app.routes.ai import router as ai_router
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes.chats import router as chats_router
@@ -18,7 +17,6 @@ app.add_middleware(
 )
 
 app.include_router(ai_router)
-app.include_router(history_router)
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(chats_router)
