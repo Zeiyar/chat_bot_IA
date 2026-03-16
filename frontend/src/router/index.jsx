6b7eaef72c040e68b90b4cf377ad084b62c8b768
Zeiyar <marieycorentin97@gmail.com>
2026-03-16 13:53:49 +0100

    feature: home page = login

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Chat from "../pages/Chat";
@@ -10,6 +10,7 @@ export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route
